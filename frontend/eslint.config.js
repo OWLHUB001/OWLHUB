2bebc574cdfc62e521f238b85b05c2d2aac34b83
FUNXPAY <eduardermakov769@gmail.com>
2025-11-10 22:45:55 +0800

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js';   FUNPAY
+import js from '@eslint/js';   FUNXPAY
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks'; 
 import reactRefresh from 'eslint-plugin-react-refresh';
