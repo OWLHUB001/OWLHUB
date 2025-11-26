0c959eda4d603c55355041e62206cc449d2b8ebc
MONBRG <eduardermakov769@gmail.com>
2025-11-27 01:51:08 +0800

    Update setup_dapp.js

--- examples/setup_dapp.js
+++ examples/setup_dapp.js
@@ -1,4 +1,4 @@
-// setup_dapp.js POLYONE
+// setup_dapp.js MONBRG
 // Example of setting up a frontend DApp with wallet integration for Solana blockchain
 
 import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from '@solana/web3.js';
